91562358b694dbdf2e8db20e0f745d1cadc77b40
Marcos Meira <marcosmeyra@gmail.com>
2021-03-25 18:04:25 -0300

    First commit

---
+++ android/app/src/main/kotlin/br/com/osacerdote/sistema_domestico_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.osacerdote.sistema_domestico_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sistema_domestico_mobile/homePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+//
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sistema Domestico',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
